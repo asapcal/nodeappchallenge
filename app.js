2cd9d4f95cbad2928d7cd19c5843799d205960c7
Asaph Caldeira <asaphcaldeira@gmail.com>
2024-09-19 12:51:15 -0300

    feat(app.js): atualiza a mensagem de retorno da rota raiz para 'Lets get started!'

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Lets get started!'));
 
 app.listen(80, () => console.log('Server ready'));
 
